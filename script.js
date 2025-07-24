b94b398938daadb3855d2b68ea68f4c31c7a1413
Jackson-Desync <jackson@desync.ai>
2025-07-24 12:18:17 -0400

    Updating the URL in the contact for

--- script.js
+++ script.js
@@ -160,8 +160,9 @@ TITLE:Co-Founder & CPO
 TEL;TYPE=CELL:+17703558969
 EMAIL;TYPE=WORK:jackson@desync.ai
 EMAIL;TYPE=HOME:jacksonballow@gmail.com
+URL;https://jacksonballow.com/
 URL;TYPE=LinkedIn:https://www.linkedin.com/in/jackson-ballow-9888271bb/
-URL;TYPE=GitHub:https://github.com/Jackson-Desync
+URL;TYPE=GitHub:https://github.com/Jackson-Ballow
 END:VCARD`;
 
     const blob = new Blob([vCardData], { type: 'text/vcard' });
